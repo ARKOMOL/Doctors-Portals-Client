8b8691b2e91b3bb58cc2b976e73140047f6c8adf
ARKOMOL <80365662+ARKOMOL@users.noreply.github.com>
2022-05-11 17:47:42 +0600

    services part

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Info from './Info';
+import clock from '../../assets/icons/clock.svg';
+import Services from './Services';
 
 const Home = () => {
     return (
-        <div>
+        <div className='px-12'>
             <h1>This is home</h1>
             <Banner/>
+            <Info/>
+            <Services/>
         </div>
     );
 };
